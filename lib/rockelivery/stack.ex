64531bc54ce9dac25c6c33a17144029ac1de812c
Guedesleo <leonardo.guedes.rocha@gmail.com>
2022-07-06 23:29:57 -0300

    Rockelivery genServer

---
+++ lib/rockelivery/stack.ex
@@ -0,0 +1,48 @@
+defmodule Rockelivery.Stack do
+  use GenServer
+
+  #CLIENT
+
+  def start_lin(initial_stack) when is_list (initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
+  #Server(Callbacks)
+
+  @impt true
+  def init(stack) do
+    {:ok, stack}
+  end
+
+  @impt true
+  #SYNC
+  def handle_call({:push, element}, _from, stack) do
+    new_stack = [element | stack]
+    {:reply, new_stack, new_stack}
+  end
+
+  @impt true
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impt true
+  def handle_call(:pop, _from, []) do
+    {:reply, nil, []}
+  end
+
+  #ASYNC
+  def handle_cast({:push, element},  stack) do
+    {:noreply, [element | stack]}
+  end
+
+
+end
