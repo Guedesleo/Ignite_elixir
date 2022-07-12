30a5f960536dcd594aa7bda76f30180479050608
Guedesleo <leonardo.guedes.rocha@gmail.com>
2022-07-12 20:18:08 -0300

    Add Credo

--- lib/rockelivery/orders/report_runner.ex
+++ lib/rockelivery/orders/report_runner.ex
@@ -1,50 +1,34 @@
 defmodule Rockelivery.Orders.ReportRunner do
-  @moduledoc """
-  This module defines the test case to be used by
-  channel tests.
-
-  Such tests rely on `Phoenix.ChannelTest` and also
-  import other functionality to make it easier
-  to build common data structures and query the data layer.
-
-  Finally, if the test case interacts with the database,
-  we enable the SQL sandbox, so changes done to the database
-  are reverted at the end of every test. If you are using
-  PostgreSQL, you can even run database tests asynchronously
-  by setting `use RockeliveryWeb.ChannelCase, async: true`, although
-  this option is not recommended for other databases.
-  """
-
   use GenServer
-  require Logger
 
   alias Rockelivery.Orders.Report
 
-  # CLIENT
+  require Logger
 
-  def start_link(_initial_stack) do
+  def start_link(_initial_state) do
     GenServer.start_link(__MODULE__, %{})
   end
 
   @impl true
   def init(state) do
-    schedule_report_generation()
+    Logger.info("ReportRunner started")
 
+    schedule_report_generation()
     {:ok, state}
   end
 
   @impl true
-  # RECEBE QUALQUER TIPO DE MENSAGEM
-  def handle_info(:generate, state) do
-    Logger.info("Report Runner Starterd....")
+  def handle_info(:generate_report, state) do
+    Logger.info("Generating report...")
 
     Report.create()
+
     schedule_report_generation()
 
     {:noreply, state}
   end
 
   def schedule_report_generation do
-    Process.send_after(self(), :generate, 1000 * 60)
+    Process.send_after(self(), :generate_report, 1000 * 10)
   end
 end
--- lib/rockelivery/via_cep/client.ex
+++ lib/rockelivery/via_cep/client.ex
@@ -1,37 +1,29 @@
 defmodule Rockelivery.ViaCep.Client do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
+  @behaviour Rockelivery.ViaCep.Behaviour
 
-  This can be used in your application as:
-
-      use RockeliveryWeb, :controller
-      use RockeliveryWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
   use Tesla
+
   alias Rockelivery.Error
-  alias Rockelivery.ViaCep.Behaviour
   alias Tesla.Env
-  @behaviour Behaviour
+
   @base_url "https://viacep.com.br/ws/"
-  plug(Tesla.Middleware.JSON)
+
+  plug Tesla.Middleware.JSON
+
+  def get_client do
+    :rockelivery
+    |> Application.fetch_env!(Rockelivery.ViaCep)
+    |> Keyword.get(:via_cep_adapter)
+  end
 
   def get_cep_info(url \\ @base_url, cep) do
-    "#{url}#{cep}/json/"
+    "#{url}#{cep}/json"
     |> get()
     |> handle_get()
   end
 
   defp handle_get({:ok, %Env{status: 200, body: %{"erro" => true}}}) do
-    {:error, Error.build(:not_found, "CEP not found!")}
+    {:error, Error.build_not_found("CEP not found")}
   end
 
   defp handle_get({:ok, %Env{status: 200, body: body}}) do
@@ -39,10 +31,10 @@ defmodule Rockelivery.ViaCep.Client do
   end
 
   defp handle_get({:ok, %Env{status: 400, body: _body}}) do
-    {:error, Error.build(:bad_request, "Invalid CEP!")}
+    {:error, Error.build_bad_request("Invalid CEP")}
   end
 
   defp handle_get({:error, reason}) do
-    {:error, Error.build(:bad_request, reason)}
+    {:error, Error.build_bad_request(reason)}
   end
 end
