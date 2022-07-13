0a6c773d53fc660dcec34b30d78ad23e031ce594
Guedesleo <leonardo.guedes.rocha@gmail.com>
2022-07-13 20:44:36 -0300

    Ajustes nos teste

--- lib/rockelivery/stack.ex
+++ lib/rockelivery/stack.ex
@@ -1,26 +1,9 @@
 defmodule Rockelivery.Stack do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use RockeliveryWeb, :controller
-      use RockeliveryWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
   use GenServer
 
-  # CLIENT
+  # Client
 
-  def start_lin(initial_stack) when is_list(initial_stack) do
+  def start_link(initial_stack) when is_list(initial_stack) do
     GenServer.start_link(__MODULE__, initial_stack)
   end
 
@@ -32,30 +15,31 @@ defmodule Rockelivery.Stack do
     GenServer.call(pid, :pop)
   end
 
-  # Server(Callbacks)
+  # Server (Callbacks)
 
-  @impt true
+  @impl true
   def init(stack) do
     {:ok, stack}
   end
 
-  @impt true
+  @impl true
   # SYNC
   def handle_call({:push, element}, _from, stack) do
     new_stack = [element | stack]
     {:reply, new_stack, new_stack}
   end
 
-  @impt true
+  @impl true
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
 
-  @impt true
+  @impl true
   def handle_call(:pop, _from, []) do
     {:reply, nil, []}
   end
 
+  @impl true
   # ASYNC
   def handle_cast({:push, element}, stack) do
     {:noreply, [element | stack]}
