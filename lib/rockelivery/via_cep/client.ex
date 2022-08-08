213ad94b97a68f07d23af5b05c43f08f994741a6
Guedesleo <leonardo.guedes.rocha@gmail.com>
2022-08-08 19:45:29 -0300

    ajustes formatted credo

--- lib/rockelivery/via_cep/client.ex
+++ lib/rockelivery/via_cep/client.ex
@@ -1,23 +1,29 @@
 defmodule Rockelivery.ViaCep.Client do
+  @behaviour Rockelivery.ViaCep.Behaviour
+
   use Tesla
 
   alias Rockelivery.Error
-  alias Rockelivery.ViaCep.Behaviour
   alias Tesla.Env
 
-  @behaviour Behaviour
-
   @base_url "https://viacep.com.br/ws/"
+
   plug Tesla.Middleware.JSON
 
+  def get_client do
+    :rockelivery
+    |> Application.fetch_env!(Rockelivery.ViaCep)
+    |> Keyword.get(:via_cep_adapter)
+  end
+
   def get_cep_info(url \\ @base_url, cep) do
-    "#{url}#{cep}/json/"
+    "#{url}#{cep}/json"
     |> get()
     |> handle_get()
   end
 
   defp handle_get({:ok, %Env{status: 200, body: %{"erro" => true}}}) do
-    {:error, Error.build(:not_found, "CEP not found!")}
+    {:error, Error.build_not_found("CEP not found")}
   end
 
   defp handle_get({:ok, %Env{status: 200, body: body}}) do
@@ -25,10 +31,10 @@ defmodule Rockelivery.ViaCep.Client do
   end
 
   defp handle_get({:ok, %Env{status: 400, body: _body}}) do
-    {:error, Error.build(:bad_request, "Invalid CEP!")}
+    {:error, Error.build_bad_request("Invalid CEP")}
   end
 
   defp handle_get({:error, reason}) do
-    {:error, Error.build(:bad_request, reason)}
+    {:error, Error.build_bad_request(reason)}
   end
 end
